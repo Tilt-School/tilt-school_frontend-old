f1d69c5ef4605d0ddb5e77a283fbadf6f0e76550
Andrii Herchykov <andriiwp3@gmail.com>
2022-04-23 23:19:49 +0300

    added base routing structure

---
+++ src/router/app-router.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { routes } from './routes';
+
+export const AppRouter: React.FC = () => (
+	<Routes>
+		{routes.map((route) => (
+			<Route key={route.path} path={route.path} element={route.element} />
+		))}
+	</Routes>
+);
