a1489144d08c103d4e07f4ae266a4be536734634
Andrii Herchykov <andriiwp3@gmail.com>
2022-04-23 23:47:21 +0300

    added layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { AppRouter } from 'src/router';
 
 const App: React.FC = () => {
-	return <div className='App'>Hello World</div>;
+	return (
+		<div className='App'>
+			<AppRouter />
+		</div>
+	);
 };
 
 export default App;
